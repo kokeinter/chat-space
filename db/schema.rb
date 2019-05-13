4bba6887bc900d1fb85168f7576bf47434be3881
こけ <koni.ken.engineer@gmail.com>
2019-05-13 10:36:19 +0900

    add column "name" To Groups

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190512083155) do
+ActiveRecord::Schema.define(version: 20190513012137) do
 
   create_table "group_users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer  "group_id"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 20190512083155) do
   create_table "groups", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "name",       null: false
   end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
