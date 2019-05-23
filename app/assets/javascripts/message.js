37ed9dab76c143541577f476ae5b24dc5fa8bb83
こけ <koni.ken.engineer@gmail.com>
2019-05-23 18:08:28 +0900

    implement auto update

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,17 +1,102 @@
 $(function(){
   function buildHTML(message){
-    var html = `<div class="main-msgs__box">
-                  <div class="main-msgs__user-name">${message.user_name}</div>
-                  <div class="main-msgs__msg-time">${message.created_at}</div>
-                  <div class="main-msgs__a-msg">
-                    <p class="lower-message__content">
-                      ${message.content}
-                    </p>
-                    <img class="main-msgs__a-msg__image" src="${message.image.url}" onerror='this.style.display = "none"'>
-                  </div>
-                </div>`
+    if (message.content && message.image.url) {
+      //data-idが反映されるようにしている
+      var html = '<div class="main-msgs__box" data-id=' + message.id + '>' +
+          '<div class="main-msgs__user-name">' +
+            message.user_name +
+          '</div>' +
+          '<div class="main-msgs__msg-time">' +
+            message.created_at +
+          '</div>' +
+          '<div class="main-msgs__a-msg">' +
+            '<p class="lower-message__content">' +
+              message.content +
+            '</p>' +
+            '<img src="' + message.image.url + '" class="main-msgs__a-msg__image" >' +
+          '</div>'
+      '</div>'
+    } else if (message.content) {
+      //同様に、data-idが反映されるようにしている
+      var html = '<div class="main-msgs__box" data-id=' + message.id + '>' +
+          '<div class="main-msgs__user-name">' +
+            message.user_name +
+          '</div>' +
+          '<div class="main-msgs__msg-time">' +
+            message.created_at +
+          '</div>' +
+        '<div class="main-msgs__a-msg">' +
+          '<p class="lower-message__content">' +
+            message.content +
+          '</p>' +
+        '</div>' +
+      '</div>'
+    } else if (message.image.url) {
+      //同様に、data-idが反映されるようにしている
+      var html = '<div class="main-msgs__box" data-id=' + message.id + '>' +
+          '<div class="main-msgs__user-name">' +
+            message.user_name +
+          '</div>' +
+          '<div class="main-msgs__msg-time">' +
+            message.created_at +
+          '</div>' +
+        '<div class="main-msgs__a-msg">' +
+          '<img src="' + message.image.url + '" class="main-msgs__a-msg__image" >' +
+        '</div>' +
+      '</div>'
+    };
     return html;
-  }
+  };
+
+  function insertHTML(message){
+    if (message.content && message.image.url) {
+      //data-idが反映されるようにしている
+      var html = '<div class="main-msgs__box" data-id=' + message.id + '>' +
+          '<div class="main-msgs__user-name">' +
+            message.user_name +
+          '</div>' +
+          '<div class="main-msgs__msg-time">' +
+            message.created_at +
+          '</div>' +
+          '<div class="main-msgs__a-msg">' +
+            '<p class="lower-message__content">' +
+              message.content +
+            '</p>' +
+            '<img src="' + message.image.url + '" class="main-msgs__a-msg__image" >' +
+          '</div>'
+      '</div>'
+    } else if (message.content) {
+      //同様に、data-idが反映されるようにしている
+      var html = '<div class="main-msgs__box" data-id=' + message.id + '>' +
+          '<div class="main-msgs__user-name">' +
+            message.user_name +
+          '</div>' +
+          '<div class="main-msgs__msg-time">' +
+            message.created_at +
+          '</div>' +
+        '<div class="main-msgs__a-msg">' +
+          '<p class="lower-message__content">' +
+            message.content +
+          '</p>' +
+        '</div>' +
+      '</div>'
+    } else if (message.image.url) {
+      //同様に、data-idが反映されるようにしている
+      var html = '<div class="main-msgs__box" data-id=' + message.id + '>' +
+          '<div class="main-msgs__user-name">' +
+            message.user_name +
+          '</div>' +
+          '<div class="main-msgs__msg-time">' +
+            message.created_at +
+          '</div>' +
+        '<div class="main-msgs__a-msg">' +
+          '<img src="' + message.image.url + '" class="main-msgs__a-msg__image" >' +
+        '</div>' +
+      '</div>'
+    };
+    return html;
+  };
+
   $('#new_message').on('submit', function(e){
     e.preventDefault();
     var formData = new FormData(this);
@@ -24,12 +109,10 @@ $(function(){
       processData: false,
       contentType: false
     })
-
-
     .done(function(data){
       var html = buildHTML(data);
       $('.main-msgs').append(html);
-      $('.form__message').reset();
+      $('.new_message')[0].reset();
       $('.form__submit').prop('disabled',false);
       $('html, body').animate({
         scrollTop: $(document).height()
@@ -38,6 +121,45 @@ $(function(){
     .fail(function(){
       alert('error');
       $('.form__submit').prop('disabled',false);
+    });
+  });
+  var reloadMessages = function() {
+    //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
+    // last_message_id = $('.main-msgs')(子要素を配列)（最後の要素だけ）.data('msg-id');
+    last_message_id = $('.main-msgs__box:last').data('id');
+    console.log(last_message_id)
+    var url =location.href
+    url = url.replace("/messages","/api/messages")
+    console.log(url)
+    $.ajax({
+      //ルーティングで設定した通りのURLを指定
+      url: url,
+      //ルーティングで設定した通りhttpメソッドをgetに指定
+      type: 'get',
+      dataType: 'json',
+      //dataオプションでリクエストに値を含める
+      data: {id: last_message_id}
     })
-  })
+    .done(function(messages) {
+      console.log('success');
+      //追加するHTMLの入れ物を作る
+      // var insertHTML = '';
+      //配列messagesの中身一つ一つを取り出し、HTMLに変換したものを入れ物に足し合わせる
+      messages.forEach(function(message){
+        //メッセージが入ったHTMLを取得
+        var html = insertHTML(message);
+        //メッセージを追加
+        $('.main-msgs').append(html);
+        // $('.form__message').reset();
+        // $('.form__submit').prop('disabled',false);
+        $('html, body').animate({
+          scrollTop: $(document).height()
+        },1500);
+      });
+    })
+    .fail(function() {
+      console.log('error');
+    });
+  };
+  setInterval(reloadMessages, 5000);
 });
